e2fe8dcfcd82ada0fd59379bccd75830c524edcd
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-05-18 03:46:13 -0300

    Completo componente MyHeader. Instalo icons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import { Switch } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function App({ Component, pageProps }) {
   return (<Component {...pageProps} />)
