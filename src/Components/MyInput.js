ab371f9ecc0750da3b3773cabaa2edfdbb289ded
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-05-19 14:35:30 -0300

    Modifico height del componente MyBanner

--- src/Components/MyInput.js
+++ src/Components/MyInput.js
@@ -3,7 +3,7 @@ import Form from 'react-bootstrap/Form';
 export default function MyInput() {
   return (
     <>
-        <Form className="mb-3" controlId="formBasicEmail">
+        <Form className="mb-3">
           <Form.Label></Form.Label>
           <Form.Control type="text" placeholder="Dirección o punto de referencia" />
         </Form>
